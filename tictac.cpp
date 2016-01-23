26d7d3d90af8a37c408a4512694a2fa2ccd847af
Russ Gould <russ.gould1@gmail.com>
2016-01-23 14:11:09 -0800

    Added first files to game.
    Added tictac.cpp and some very rudimentary set-up code.
    Added tictac.h and a few include statements to get started.

---
+++ tictac.cpp
@@ -0,0 +1,14 @@
+// tic-tac-toe game project to test collaboration.
+// here is a kirby: <(0.0)>
+
+#include "tictac.h"
+using namespace std;
+
+int main()
+{
+    cout << "This is a badass tic-tac-toe game." << endl;
+
+    //add some code and stuff.
+
+    return 0;
+}
---
+++ tictac.h
@@ -0,0 +1,11 @@
+/*  =======================================================
+    This is the header file for our tic-tac-toe game.
+
+    Dan, please add any include statements that I missed
+    =======================================================
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
