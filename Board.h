e0e35d2221e76605f49a55cc2d967dd677072ba2
dxh5314 <danieldhales@gmail.com>
2016-02-17 19:23:17 -0500

    Update Board.h
    updated to reflect changes in board.cpp
    
    added a boolean called "winner" so i wouldn't have to have weird functions floating around doing the same thing

--- Board.h
+++ Board.h
@@ -8,18 +8,20 @@ class Board
     friend class Line;
     private:
         char grid[3][3]; // holds the moves;
-        bool updateLines();
-        bool checkLines();
+        void updateLines(); // updates all the line objects
+        void checkLines();
         bool isSpaceEmpty( char mov[2] );
         Line R1, R2, R3, C1, C2, C3, D1, D2;
+        bool winner;
 
 
     public:
         Board();
         void printGrid();
         void printLines();
-        bool makeMove(char mv[2], char player);
+        void makeMove(char mv[2], char player);
         bool isInputOkay(char mov[2]);
+        bool isWinner();
 
 };
 
