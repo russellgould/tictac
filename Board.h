d2adfff2af41cfbe8135d330cb5aa96f44c794a8
dxh5314 <danieldhales@gmail.com>
2016-03-06 21:38:04 -0500

    added headers for the new functions

--- Board.h
+++ Board.h
@@ -6,14 +6,14 @@
 class Board
 {
     friend class Line;
-    private:
-        char grid[3][3]; // holds the moves;
-        void updateLines(); // updates all the line objects
-        void checkLines();
-        bool isSpaceEmpty( char mov[2] );
-        Line R1, R2, R3, C1, C2, C3, D1, D2;
-        bool winner;
 
+    private:
+        char grid[3][3]; // holds the actual grid
+        void updateLines(); // updates all the line objects
+        void checkLines(); // checks the line objects for a winner
+        bool isSpaceEmpty( char mov[2] ); // checks the board to see if a move is valid
+        Line R1, R2, R3, C1, C2, C3, D1, D2; // line objects for the board
+        bool winner; // true = someone won. false = nope
 
     public:
         Board();
@@ -22,6 +22,8 @@ class Board
         void makeMove(char mv[2], char player);
         bool isInputOkay(char mov[2]);
         bool isWinner();
+        void computersMove(char);
+        char whoWon();
 
 };
 
